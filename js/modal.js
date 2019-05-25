2f83d4c74be0f07b64599aeb6ea7ab9f665c166a
keats0108 <jinho.hong@dulwich-beijing.cn>
2019-05-25 11:39:29 +0800

    Modal added

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+//Open
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+//Close
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
