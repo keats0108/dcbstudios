52e06330b1983a38cf75c73417fd038930df0e31
Mark Li <mark.haoxiang@gmail.com>
2019-05-26 15:29:16 +0800

    Name change

--- studoSite/studoSite/urls.py
+++ studoSite/studoSite/urls.py
@@ -1,4 +1,4 @@
-"""studioApp URL Configuration
+"""studoSite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
