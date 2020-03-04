5a444d2aace786a95e7af572e7a0c8fd29af03af
keats0108 <jinho.hong@dulwich-beijing.cn>
2020-03-04 11:53:50 +0800

    Hopefully?

--- js/buttons.js
+++ js/buttons.js
@@ -1,51 +1,69 @@
+var desc = document.getElementsByClassName("content");
 var x = document.getElementById("opened1");
 var y = document.getElementById("opened2");
 var z = document.getElementById("opened3");
+var a = document.getElementById("opened4");
         
 function open1(){
     if (x.style.display === "none") {
         x.style.display = "block";
         y.style.display = "none";
         z.style.display = "none";
+        a.style.display = "none";
     } else {
         x.style.display = "none";
         y.style.display = "none";
         z.style.display = "none";
+        a.style.display = "none";
     }
 }
     
 function open2(){
-    if (x.style.display === "none") {
+    if (y.style.display === "none") {
         x.style.display = "none";
         y.style.display = "block";
         z.style.display = "none";
+        a.style.display = "none";
     } else {
         x.style.display = "none";
         y.style.display = "none";
         z.style.display = "none";
+        a.style.display = "none";
     }
 }
             
 function open3(){
-    if (x.style.display === "none") {
+    if (z.style.display === "none") {
         x.style.display = "none";
         y.style.display = "none";
         z.style.display = "block";
+        a.style.display = "none";
     } else {
         x.style.display = "none";
         y.style.display = "none";
         z.style.display = "none";
+        a.style.display = "none";
+    }
+}
+            
+function open4(){
+    if (a.style.display === "none") {
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        a.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+        a.style.display = "none";
     }
 }
             
 function close(){
-    if (x.style.display === "block" || y.style.display === "block" || z.style.display === "block") {
-        x.style.display = "none";
-        y.style.display = "none";
-        z.style.display = "none";
+    if (desc.style.display === "block") {
+        desc.style.display = "none";
     } else {
-        x.style.display = "none";
-        y.style.display = "none";
-        z.style.display = "none";
+        desc.style.display = "none";
     }
 }
